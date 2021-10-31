331b2475bd3d5029e9fe0942fbd805809cedb34c
riyadhswe <riyadhswe@gmail.com>
2021-10-31 14:58:39 +0600

    new data

--- app/views.py
+++ app/views.py
@@ -10,6 +10,13 @@ def about(request):
     return render(request,'about.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        contactdata = contact(name=name, email=email, subject=subject, message=message)
+        contactdata.save()
     return render(request,'contact.html')
 
 def portfolio(request):
